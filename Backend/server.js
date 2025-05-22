eaad01fad98f40ed1ae1caab50b23508f2a9b17a
Ubuntu <ubuntu@ip-172-31-37-103.ec2.internal>
2025-05-22 09:35:03 +0000

    Initial commit of ajay_Employee_Benefits_AjayKumar project

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,10 @@ const { Pool } = require('pg');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
 
 // PostgreSQL connection
+app.use(cors());
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
